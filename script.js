04ec64ee54f63fffb4d729b4024e3432c78ae6f9
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-28 18:44:24 -0600

    working on alien class

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ canvas.height = 400;
 ctx.font = "25px Impact";
 
 class Alien {
-  constructor() {
+  constructor(game) {
+    this.game = game;
     this.x = 50;
     this.y = 50;
     this.width = 50;
@@ -21,7 +22,7 @@ class Game {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
-    this.alien = new Alien();
+    this.alien = new Alien(this);
   }
   render(context) {
     this.alien.draw(context);
