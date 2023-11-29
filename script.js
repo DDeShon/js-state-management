03af6f6ddff2078bdd99f808103e1ef83a847dd2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-28 18:41:07 -0600

    working on game class

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 400;
+ctx.font = "25px Impact";
+
+class Alien {}
+
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.alien = new Alien();
+  }
+}
